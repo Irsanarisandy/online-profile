17f8b2dbd32b25a89481f91796894f7486eb5317
Irsan Arisandy <irsanarisandy@hotmail.com>
2018-02-23 21:58:45 +1300

    Change icons and initial text

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './thinking.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Irsan Arisandy</h1>
+                </header>
+                <p className="App-intro">
+                    Future Software/Web Developer, meme-lord
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
