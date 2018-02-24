91d028494bdbf36ae1468f4a10ad75a8062941b7
Irsan Arisandy <irsanarisandy@hotmail.com>
2018-02-24 22:48:52 +1300

    Add drawer to navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import SvgIcon from 'material-ui/SvgIcon';
-import FlatButton from 'material-ui/FlatButton';
-import logo from '../assets/thinking.svg';
+import {AppBar, Drawer, FlatButton, IconButton, MenuItem, SvgIcon} from 'material-ui';
 import ActionHome from 'material-ui/svg-icons/action/home';
+import logo from '../assets/thinking.svg';
 import '../styles/Home.css';
 
 const GitHubIcon = (props) => (
@@ -14,32 +11,73 @@ const GitHubIcon = (props) => (
     </SvgIcon>
 );
 
-const LinkedIn = (props) => (
+const LinkedInIcon = (props) => (
     <SvgIcon {...props}>
         <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
     </SvgIcon>
 );
 
 const RightIcons = () => (
-    <div class="navIcons">
-        <FlatButton label="Home"/>
-        <FlatButton label="Projects"/>
-        <FlatButton label="Contact"/>
+    <div className="navIcons">
+        <FlatButton label="Home" />
+        <FlatButton label="Projects" />
+        <FlatButton label="Contact" />
         <IconButton href="https://github.com/irsanarisandy">
-            <GitHubIcon/>
+            <GitHubIcon />
         </IconButton>
         <IconButton href="https://nz.linkedin.com/in/irsan-arisandy-72008b117">
-            <LinkedIn/>
+            <LinkedInIcon />
         </IconButton>
     </div>
 );
 
-class App extends React.Component {
+class CustomNavbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+        this.handleToggle = this.handleToggle.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleToggle() {
+        this.setState({open: !this.state.open});
+    }
+
+    handleClose() {
+        this.setState({open: false});
+    }
+
+    render() {
+        return (
+            <div>
+                <Drawer docked={false} width={200} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+                    <h3>Please Hire Me</h3>
+                    <MenuItem onClick={this.handleClose}>Home</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Projects</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Contacts</MenuItem>
+                    <IconButton href="https://github.com/irsanarisandy">
+                        <GitHubIcon />
+                    </IconButton>
+                    <IconButton href="https://nz.linkedin.com/in/irsan-arisandy-72008b117">
+                        <LinkedInIcon />
+                    </IconButton>
+                </Drawer>
+                <AppBar id="navbarShort" className="navbar" title="Please Hire Me" onLeftIconButtonClick={this.handleToggle} />
+                <AppBar id="navbarFull" className="navbar" title="Please Hire Me" iconElementLeft={<div/>} iconElementRight={<RightIcons/>} />
+            </div>
+        );
+    }
+}
+
+export default class App extends React.Component {
     render() {
         return (
             <MuiThemeProvider>
             <div className="App">
-                <AppBar id="navbar" title="Please Hire Me" iconElementRight={RightIcons()} />
+                {/* <Drawer docked={false} width={200} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+                    <MenuItem onClick={this.handleClose}>Home</MenuItem>
+                </Drawer> */}
+                <CustomNavbar />
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Irsan Arisandy</h1>
@@ -53,5 +91,3 @@ class App extends React.Component {
         );
     }
 }
-
-export default App;
