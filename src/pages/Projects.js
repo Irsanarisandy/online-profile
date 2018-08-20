6a303997b716a78f77e4477cc80a63457f263bc9
Irsan Arisandy <irsanarisandy@hotmail.com>
2018-08-20 15:42:37 +1200

    Add web application project: Eat My App.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,6 +5,7 @@ import wellness from '../assets/wellness-shed.png';
 import aucs from '../assets/aucs.png';
 import etite from '../assets/app-etite.png';
 import qwiky from '../assets/qwiky.png';
+import eatMyApp from '../assets/eat-my-app.png';
 import currency from '../assets/currency-converter.png';
 import '../styles/projects.css';
 
@@ -28,6 +29,9 @@ export default () => (
                 </a>
                 <a href="https://badboiz.github.io/frontend/" target="_blank" rel="noopener noreferrer">
                     <img src={qwiky} alt="Qwiky" />
+                </a>
+                <a href="http://eatmyapp.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={eatMyApp} alt="Eat My App" />
                 </a>
             </div>
             <h3>Mobile Applications:</h3>
